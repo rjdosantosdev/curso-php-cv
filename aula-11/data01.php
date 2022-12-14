32e04d38724e7fb57ac30816c32fd28ae209027e
romariojdosantos <romario.santos@ambisis.com.br>
2022-12-14 17:20:31 -0300

    exercicios de while

---
+++ aula-11/data01.php
@@ -0,0 +1,12 @@
+<?php 
+
+	$num = $_GET['contadordinamico'];
+
+	$counter = 1;
+	while ($num >= $counter) {
+		echo "<span>&nbsp;$counter&nbsp;-</span>";
+	$counter++;
+	}
+
+	# 5 é maior que 1?
+?>
--- aula-11/index.php
+++ aula-11/index.php
@@ -6,13 +6,41 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Estrutura de Repetição While</title>
 </head>
+<style type="text/css">
+  h1 {
+    display: inline-block;
+  }
+</style>
 <body>
     <?php 
+      // com var iniciando no inicio
       $a = 1;
-      while ($a <= 10) {
-       echo "<h1>$a | </h1>";
+      while ($a <= 10) { 
+       echo "<h1>&nbsp;$a&nbsp;-</h1>";
        $a++;
       }
+      ?>
+     
+    <p><br>======================</p>
+    <?php 
+       $b = 10;
+      while ($b >= 1) {
+        echo "<h1>&nbsp;$b&nbsp;-</h1>";
+        $b--;
+      }
     ?>
+
+    <form method="get" action="./data01.php">
+        <label for="contadordinamico">Contador dinamico</label><br>
+        <?php 
+          
+          $contador = 1;
+          while ($contador <= 5) {
+            echo "<br>Valor $contador:<input max='30' min='0' type='number' name='contadordinamico'>";
+            $contador++;
+         } 
+        ?>
+        <input type="submit" value="Verificar">
+    </form>
 </body>
 </html>
